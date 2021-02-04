eaf34c072059c43346cd69bf31de1073843ad620
Daichiryuu <4687778@gmail.com>
2021-02-04 15:38:26 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# TheBigBangTheory_Randomizer
+Запускает рандомную серию сериала "Теория большого взрыва" на сервисе kinopoisk.ru
+
+Important: Для просмотра нужна будет подписка кинопоиск.
+
+
+Usage:
+1. Скачиваем приложение по [ссылке](https://github.com/DaichiRyuu/TheBigBangTheory_Randomizer/releases/download/1.0/TheBigBangTeory_Randomizer.exe)
+2. Запускаем двойным нажатием на иконку
+3. Наслаждаемся :)
---
+++ main.py
@@ -0,0 +1,31 @@
+from random import randrange
+from sys import exit as sys_exit
+from webbrowser import open as browser_open
+
+
+def get_season():
+    se = randrange(1, 12, 1)
+    return se
+
+
+def get_episode(season_num):
+    if season_num == 1:
+        ep = randrange(1, 17, 1)
+    elif 1 < season_num < 4:
+        ep = randrange(1, 23, 1)
+    else:
+        ep = randrange(1, 24, 1)
+    return ep
+
+
+def get_url(season_num, episode_num):
+    url = f'https://hd.kinopoisk.ru/film/4c96bc227a377c8aa98da3789416c811?episode={episode_num}&from_block=kp-button-online&season={season_num}&watch='
+    return url
+
+
+if __name__ == '__main__':
+    # pyinstaller --onefile --nowindowed --icon "TBBT.ico" --name "TheBigBangTeory_Randomizer" main.py
+    season = get_season()
+    episode = get_episode(season)
+    browser_open(get_url(season, episode))
+    sys_exit()
